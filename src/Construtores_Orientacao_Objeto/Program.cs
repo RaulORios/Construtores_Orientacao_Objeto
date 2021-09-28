83fa4336dde35a0ffc553a7ca82b3762a739ddc4
RaulORios <raul.orios89@outlook.com>
2021-09-28 10:39:35 -0300

    Arquivos atualizados

---
+++ src/Construtores_Orientacao_Objeto/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace Construtores_Orientacao_Objeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                                               
+            Console.WriteLine("Entre os dados do produto >>>");
+            Console.Write("Nome: ");
+            string nome = (Console.ReadLine());
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Produto p = new Produto(nome, preco, quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine($"Dados do produto: {p}");
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qtde = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qtde);
+
+            Console.WriteLine();
+            Console.WriteLine($"Dados atualizados: {p}");
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qtde = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qtde);
+
+            Console.WriteLine();
+            Console.WriteLine($"Dados atualizados: {p}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
